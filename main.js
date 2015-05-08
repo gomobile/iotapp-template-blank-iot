a85c01c4a1493bf19fa8102adbaf9850c11916ac
eashtian <eashtian@EASHTIAN-MOBL1.amr.corp.intel.com>
2015-05-08 14:18:25 -0700

    update with JSHint directives

--- main.js
+++ main.js
@@ -1,2 +1,4 @@
-/*Blank Node.js IoT  project*/
+/*jslint node:true, vars:true, bitwise:true, unparam:true */
+/*jshint unused:true */
+// Leave the above lines for propper jshinting
 //Type Node.js Here :)
